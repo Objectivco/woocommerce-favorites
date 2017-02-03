693b403f08bba91414368bcf1e39caca1c337d86
Wesley Cole <wes@cgd.io>
2017-02-02 22:00:06 -0500

    getting the base ajax set up

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,18 @@
 jQuery(document).ready(function() {
-    jQuery('.favorite-product > a').on('click', function() {
-        
+    jQuery('.favorite-product > a').on('click', function(e) {
+        e.preventDefault();
+        var postId = jQuery(this).data('product-id');
+        console.log(postId);
+        jQuery.ajax({
+            url: favorite_data.ajax_url,
+            type: 'post',
+            data: {
+                action: 'favorite_callback',
+                postId: postId
+            },
+            success: function(response) {
+                console.log(response);
+            }
+        });
     });
 });
--- includes/obj-class-main.php
+++ includes/obj-class-main.php
@@ -9,8 +9,8 @@ class Obj_Main {
     public function __construct() {
         add_action( 'wp_enqueue_scripts', array( $this, 'scripts' ) );
         add_action( 'woocommerce_before_shop_loop_item_title', array( $this, 'cgd_add_product_heart' ), 15 );
-        add_action( 'wp_ajax_favorite_callback', 'favorite_callback' );
-        add_action( 'wp_ajax_nopriv_favorite_callback', 'favorite_callback' );
+        add_action( 'wp_ajax_favorite_callback', array( $this, 'favorite_callback' ) );
+        add_action( 'wp_ajax_nopriv_favorite_callback', array( $this, 'favorite_callback' ) );
     }
 
     /**
@@ -30,7 +30,9 @@ class Obj_Main {
      */
     public function cgd_add_product_heart() {
         global $product;
-    	echo '<div class="favorite-product"><a rel="nofollow" href="#" data-product-id="' . $product->ID . '"><img src="' . get_stylesheet_directory_uri() . '/assets/images/heart.png"></a></div>';
+        $saved_products = get_user_meta( get_current_user_id(), 'saved_products', false );
+        print_r($saved_products);
+    	echo '<div class="favorite-product"><a rel="nofollow" href="#" data-product-id="' . $product->id . '"><img src="' . get_stylesheet_directory_uri() . '/assets/images/heart.png"></a></div>';
     }
 
     /**
@@ -39,7 +41,15 @@ class Obj_Main {
      * @since 1.0
      */
     public function favorite_callback() {
+        $new_product_id = $_POST['product_id'];
+        $user_id = get_current_user_id();
+        $saved_products = get_user_meta( $user_id, 'saved_products', false );
 
+        update_user_meta( $user_id, 'saved_products', $new_product_id );
+
+        echo 'done';
+
+        die();
     }
 
 }
